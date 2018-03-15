701fa7091042c9fc16e24a58f246a061ef30bd49
Shoan Chikato <sjchikato@gmail.com>
2018-03-15 23:21:09 +0200

    add models/*, add seeds.js

--- app.js
+++ app.js
@@ -1,55 +1,15 @@
 var express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
-    mongoose   = require("mongoose")
+    mongoose   = require("mongoose"),
+    Campground = require("./models/campground"),
+    seedDB     = require("./seeds")
 
 mongoose.connect("mongodb://127.0.0.1/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
-
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema ({
-	name: String,
-	image: String,
-	description: String,
-	file: Buffer
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-// 	{
-// 		name: "Granite Hill",
-// 		image: "http://localhost:3000/pics/IMG_2664.JPG",
-// 		description: "This a huge granite hill, no bathrooms. No water. Beautiful granite!"
-
-// 	}, 
-// 	function(err, campground){
-// 		if(err){
-// 			console.log(err);
-// 		} else {
-// 			console.log("NEWLY CREATED CAMPGROUND: ");
-// 			console.log(campground);
-// 		}
-// 	});
-
-
-
-
-// var campgrounds = [
-// 		{name: "Salmon Creek", image: "http://localhost:3000/pics/IMG_2663.JPG"},
-// 		{name: "Granite Hill", image: "http://localhost:3000/pics/IMG_2664.JPG"},
-// 		{name: "Mountain Goat's Rest", image: "http://localhost:3000/pics/IMG_2665.JPG"},
-// 		{name: "Salmon Creek", image: "http://localhost:3000/pics/IMG_2663.JPG"},
-// 		{name: "Granite Hill", image: "http://localhost:3000/pics/IMG_2664.JPG"},
-// 		{name: "Mountain Goat's Rest", image: "http://localhost:3000/pics/IMG_2665.JPG"},
-// 		{name: "Salmon Creek", image: "http://localhost:3000/pics/IMG_2663.JPG"},
-// 		{name: "Granite Hill", image: "http://localhost:3000/pics/IMG_2664.JPG"},
-// 		{name: "Mountain Goat's Rest", image: "http://localhost:3000/pics/IMG_2665.JPG"}
-// 	]
-
-
+seedDB();
 
 app.get("/", function(req, res){
 	res.render("landing");
@@ -95,10 +55,11 @@ app.get("/campgrounds/new", function(req, res){
 // SHOW - shows more info about one campground  
 app.get("/campgrounds/:id", function(req, res){
 	// find campground with provided ID
-	Campground.findById(req.params.id, function(err, foundCampground){
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
 		if(err){
 			console.log(err);
 		} else {
+			console.log(foundCampground);
 			// render show template with that campground
 			res.render("show", {campground: foundCampground});
 		}
